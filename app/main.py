ecb8a60ac1049b61fc9c4d03b4793d5fb0d3da0b
Vinicius <especoto.vini@gmail.com>
2023-11-06 21:43:14 -0300

    FEAT: alterando main

--- app/main.py
+++ app/main.py
@@ -17,39 +17,30 @@ jaeger_exporter = JaegerExporter(
 )
 
 span_processor = SimpleSpanProcessor(jaeger_exporter)
-
 tracer_provider.add_span_processor(BatchSpanProcessor(ConsoleSpanExporter()))
-tracer_provider.add_span_processor(span_processor)
+# tracer_provider.add_span_processor(span_processor)
 trace.set_tracer_provider(tracer_provider)
 
-# Método de contexto do trace com um decorator
-def trace_context_method(span_name):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            with trace.get_tracer(__name__).start_as_current_span(span_name):
-                result = func(*args, **kwargs)
-                return result
-        return wrapper
-    return decorator
-
+tracer = trace.get_tracer(__name__)
 
 # Função para realizar somas matemáticas
-@trace_context_method("soma")
+@tracer.start_as_current_span("sum")
 def somar(a, b):
     resultado = a + b
     return subtrair(resultado, 10)
 
-@trace_context_method("sub")
+@tracer.start_as_current_span("sub")
 def subtrair(a, b):
     return a - b
 
-if __name__ == "__main__":
-    resultado = somar(10, 20)
-    resultado = somar(10, 20)
-    resultado = somar(10, 20)
-    resultado = subtrair(10, 20)
-    resultado = subtrair(10, 20)
-    resultado = somar(10, 20)
-    print(f"Resultado da soma: {resultado}")
 
-# Certifique-se de configurar o ambiente com as variáveis de ambiente necessárias para o console exporter.
+if __name__ == "__main__":
+    # resultado = somar(10, 20)
+    # resultado = somar(10, 20)
+    # resultado = somar(10, 20)
+    resultado = subtrair(10, 20)
+    resultado = subtrair(10, 20)
+    # resultado = somar(10, 20)
+
+    
+
